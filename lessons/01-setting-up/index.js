1365c7c677fc9da5a7c3bdf92a50e7c9889a43ea
Hilary Lewis <hilarylewis92@gmail.com>
2016-11-15 16:01:00 -0700

    React router starter kit

--- lessons/01-setting-up/index.js
+++ lessons/01-setting-up/index.js
@@ -1,4 +1,35 @@
+// import React from 'react'
+// import { render } from 'react-dom'
+// import App from './modules/App'
+// // render(<App/>, document.getElementById('app'))
+//
+// import { Router, Route, browserHistory, IndexRoute } from 'react-router'
+//
+// import Home from './modules/Home'
+// import About from './modules/About'
+// import Repos from './modules/Repos'
+// import Repo from './modules/Repo'
+//
+// render((
+//   <Router history={browserHistory}>
+//     <Route path="/" component={App}>
+//
+//       <IndexRoute component={Home}/>
+//
+//       <Route path="/repos" component={Repos}>
+//         <Route path="/repos/:userName/:repoName" component={Repo}/>
+//       </Route>
+//       <Route path="/about" component={About}/>
+//     </Route>
+//   </Router>
+// ), document.getElementById('app'))
+
 import React from 'react'
 import { render } from 'react-dom'
-import App from './modules/App'
-render(<App/>, document.getElementById('app'))
+import { Router, browserHistory } from 'react-router'
+import routes from './modules/routes'
+
+render(
+  <Router routes={routes} history={browserHistory}/>,
+  document.getElementById('app')
+)
